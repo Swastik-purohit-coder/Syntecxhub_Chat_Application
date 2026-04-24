5d405c3e6696253aee4d1ca4841351970f0c64d3
swastik <swastikpurohit2007@gmail.com>
2026-04-25 03:00:12 +0530

    update origin

--- server/server.js
+++ server/server.js
@@ -13,7 +13,7 @@ const PORT = process.env.PORT || 5000;
 const app = express();
 
 app.use(cors({
-  origin: "https://your-frontend.vercel.app",
+  origin: "https://syntecxhub-chat-application.vercel.app/",
   methods: ["GET", "POST"],
   credentials: true
 }));
